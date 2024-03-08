d8bf35982a13cc8c670ef8805377b5eb2744f850
marchenkko <y237501@gmail.com>
2024-03-08 03:24:02 +0200

    Add test for LibraryManager and LibraryStatisticsAnalyzer

--- nunit-csharp-samples/Tests/LibraryManagerTests.cs
+++ nunit-csharp-samples/Tests/LibraryManagerTests.cs
@@ -1,6 +1,52 @@
-namespace nunit_csharp_samples.Tests;
-
-public class LibraryManagerTests
+namespace nunit_csharp_samples.Tests
 {
-    // TODO step 4.
+    [TestFixture]
+    public class LibraryManagerTests
+    {
+        [Test]
+        public void AddBook_BookNotInLibrary_ShouldAddBook()
+        {
+            LibraryManager manager = new LibraryManager();
+            Book book = new Book { Title = "Sample Book", Author = "Sample Author", Articul = "123456" };
+
+            manager.AddBook(book);
+
+            Assert.Contains(book, manager.GetBooks());
+        }
+
+        [Test]
+        public void AddBook_BookAlreadyInLibrary_ShouldNotAddBook()
+        {
+            LibraryManager manager = new LibraryManager();
+            Book book = new Book { Title = "Sample Book", Author = "Sample Author", Articul = "123456" };
+
+            manager.AddBook(book);
+            manager.AddBook(book);
+
+            Assert.AreEqual(1, manager.GetBooks().Count);
+        }
+
+        [Test]
+        public void RemoveBook_BookInLibrary_ShouldRemoveBook()
+        {
+            LibraryManager manager = new LibraryManager();
+            Book book = new Book { Title = "Sample Book", Author = "Sample Author", Articul = "123456" };
+            manager.AddBook(book);
+
+            manager.RemoveBook(book);
+
+            Assert.IsEmpty(manager.GetBooks());
+        }
+
+        [Test]
+        public void RemoveBook_BookNotInLibrary_ShouldNotRemoveBook()
+        {
+            LibraryManager manager = new LibraryManager();
+            Book book = new Book { Title = "Sample Book", Author = "Sample Author", Articul = "123456" };
+
+            manager.RemoveBook(book);
+
+            Assert.IsEmpty(manager.GetBooks());
+        }
+    }
 }
--- nunit-csharp-samples/Tests/LibraryStatisticsAnalyzerTests.cs
+++ nunit-csharp-samples/Tests/LibraryStatisticsAnalyzerTests.cs
@@ -1,6 +1,50 @@
 namespace nunit_csharp_samples.Tests;
 
+[TestFixture]
 public class LibraryStatisticsAnalyzerTests
 {
-    // TODO step 4.
+    [Test]
+    public void GetBooksByAuthor_AuthorExists_ShouldReturnBooks()
+    {
+        LibraryManager manager = new LibraryManager();
+        LibraryStatisticsAnalyzer analyzer = new LibraryStatisticsAnalyzer(manager);
+        Book book1 = new Book { Title = "Book 1", Author = "Author 1", Articul = "111" };
+        Book book2 = new Book { Title = "Book 2", Author = "Author 1", Articul = "222" };
+        manager.AddBook(book1);
+        manager.AddBook(book2);
+
+        var result = analyzer.GetBooksByAuthor("Author 1");
+
+        Assert.AreEqual(2, result.Count);
+        Assert.Contains(book1, result);
+        Assert.Contains(book2, result);
+    }
+
+    [Test]
+    public void GetBookByTitle_TitleExists_ShouldReturnBook()
+    {
+        LibraryManager manager = new LibraryManager();
+        LibraryStatisticsAnalyzer analyzer = new LibraryStatisticsAnalyzer(manager);
+        Book book = new Book { Title = "Sample Book", Author = "Sample Author", Articul = "123456" };
+        manager.AddBook(book);
+
+        var result = analyzer.GetBookByTitle("Sample Book");
+
+        Assert.IsNotNull(result);
+        Assert.AreEqual(book, result);
+    }
+
+    [Test]
+    public void GetBookByArticul_ArticulExists_ShouldReturnBook()
+    {
+        LibraryManager manager = new LibraryManager();
+        LibraryStatisticsAnalyzer analyzer = new LibraryStatisticsAnalyzer(manager);
+        Book book = new Book { Title = "Sample Book", Author = "Sample Author", Articul = "123456" };
+        manager.AddBook(book);
+
+        var result = analyzer.GetBookByArticul("123456");
+
+        Assert.IsNotNull(result);
+        Assert.AreEqual(book, result);
+    }
 }
